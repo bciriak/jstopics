d62edf0864cdf897f698429369d8cbaaee839968
Brano Ciriak <brano.ciriak@gmail.com>
2022-06-05 10:20:23 +0200

    Added sitemap generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+  generateRobotsTxt: true, // (optional)
+  changefreq: 'weekly',
+}
+
+module.exports = config
